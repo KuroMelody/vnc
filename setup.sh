585e9b53e2c764979cfa0c34df55ecc573505174
KuroMelody <79881740+KuroMelody@users.noreply.github.com>
2021-06-16 18:33:22 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y fluxbox firefox chromium-browser xfce4-terminal thunar mousepad nano curl wget ssh git hsetroot xarchiver build-essential gdebi lxappearance htop x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 libpam0g libxt6 libxext6
+sudo apt -qq update > /dev/null
+sudo apt -qq install -y fluxbox firefox chromium-browser xfce4-terminal thunar mousepad nano curl wget ssh git hsetroot xarchiver build-essential gdebi lxappearance htop x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 libpam0g libxt6 libxext6 > /dev/null
 cp -R .resource/.fluxbox $HOME
 
 # vnc
-wget https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
-sudo dpkg -i turbovnc_2.2.5_amd64.deb
+wget -q https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
+sudo dpkg -i turbovnc_2.2.5_amd64.deb > /dev/null
 
 export PATH=$PATH:/opt/TurboVNC/bin
 mkdir $HOME/.vnc
@@ -17,7 +17,7 @@ chmod 0600 $HOME/.vnc/passwd
 /opt/TurboVNC/bin/vncserver :1 -geometry 1280x768 -depth 16 -rfbport 7582
 
 # ngrok
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 ./ngrok authtoken $NGROK_TOKEN
