9e89a1eabb475ea68108f62a731cc80751a5d4d5
KuroMelody <79881740+KuroMelody@users.noreply.github.com>
2021-06-17 05:15:53 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,30 +2,41 @@
 
 echo "========================"
 echo "install package"
+echo "========================"
+
 sudo apt -qq update > /dev/null
-sudo apt -qq install -y fluxbox firefox chromium-browser xfce4-terminal thunar mousepad nano curl wget ssh git hsetroot xarchiver build-essential gdebi lxappearance htop x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 libpam0g libxt6 libxext6 > /dev/null
+sudo apt -qq install -y fluxbox firefox chromium-browser xfce4-terminal thunar mousepad nano curl wget ssh git hsetroot xarchiver build-essential gdebi lxappearance tigervnc-standalone-server tigervnc-xorg-extension > /dev/null
 cp -R ./resource/.fluxbox $HOME
 
 # vnc
 echo "========================"
 echo "install vnc"
-wget -q https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
-sudo dpkg -i turbovnc_2.2.5_amd64.deb > /dev/null
+echo "========================"
 
-export PATH=$PATH:/opt/TurboVNC/bin
+# wget -q https://phoenixnap.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
+# sudo dpkg -i turbovnc_2.2.5_amd64.deb > /dev/null
+
+# export PATH=$PATH:/opt/TurboVNC/bin
 mkdir $HOME/.vnc
-cp ./resource/xstartup $HOME/.vnc/xstartup.turbovnc
-sudo chmod +x $HOME/.vnc/xstartup.turbovnc
+cp ./resource/xstartup $HOME/.vnc/xstartup
+sudo chmod +x $HOME/.vnc/xstartup
 echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
 chmod 0600 $HOME/.vnc/passwd
 
-/opt/TurboVNC/bin/vncserver :1 -geometry 1280x768 -depth 16 -rfbport 7582
+vncserver :1 -geometry 1280x768 -rfbport 7582
+# /opt/TurboVNC/bin/vncserver :1 -geometry 1280x768 -depth 16 -rfbport 7582
+
+echo "========================"
+echo "log vnc"
+echo "========================"
 
 cat $HOME/.vnc/*.log
 
 # ngrok
 echo "========================"
 echo "install ngrok"
+echo "========================"
+
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
